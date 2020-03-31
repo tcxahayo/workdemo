fc0ccdb24f119ca47d9556a77261524a022d454a
唐彩霞 <tangcaixia@tangcaixiadeMac-mini.local>
2020-03-31 17:04:21 +0800

    添加本地缓存

--- src/page/App.js
+++ src/page/App.js
@@ -1,21 +1,41 @@
-import React , { useState}from 'react';
+import React , { useState,useEffect}from 'react';
 import './app.scss';
 import InputBox from './input/input';
 import ListBox from './list/list';
+import Login from './login/login';
 
 function App() {
   const [data, setData] = useState([]);
+  const [localData, setLocalData] = useState([]);
 
+  //获取input值
   const getValue = (value)=> {
     setData((data)=>{
       return data.concat(value);
     })
   }
+  //删除数据
   const delData = (index) =>{
-    let newData = [...data];
+    let newData = [...data];  //数据不扩展开另存储，页面就不会重新渲染
     newData.splice(index,1);
     setData(newData);
   }
+
+  //使用本地存储，使刷新的时候数据不会丢失
+  useEffect(()=>{
+    if(data.length > 0){
+      localStorage.setItem('data',data)
+    }
+  },[data])
+  useEffect(()=>{
+    const data1 = localStorage.getItem('data');
+    if(data1){
+      setData((data)=>{
+        return data.concat(data1.split(','));
+      })
+    }
+  },[])
+
 
   return (
     <div className="App">
---
+++ src/page/login/login.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+import './login.scss';
+
+const Login = ()=> {
+    return(
+        <div className="login">
+            123
+        </div>
+    )
+}
+export default Login;
