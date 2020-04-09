4e8588c987be447b303f1dc33540e050b15143fc
唐彩霞 <tangcaixia@tangcaixiadeMac-mini.local>
2020-04-09 18:27:31 +0800

    不知道改什么的提交

--- src/page/input/input.js
+++ src/page/input/input.js
@@ -1,37 +1,37 @@
-import React,{Component} from 'react';
-import { Input,Button } from 'antd';
+import React, { Component } from 'react';
+import { Input, Button } from 'antd';
 import "./input.scss";
 import { connect } from 'react-redux';
-import { valueAction,addAction } from '../../store/actionCreate';
-import {bindActionCreators} from 'redux';
+import { valueAction, addAction } from '../../store/actionCreate';
+import { bindActionCreators } from 'redux';
 
 class InputBox extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render(){
-        const {value, addAction, valueAction} = this.props;
-        return(
-             <div className="inputBox"> 
-             <Input placeholder="请输入待办事项"  className="input" value={value} onChange={valueAction} />
-             <Button type="primary" shape="round" onClick={addAction}  >确定</Button>
-         </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const { value, addAction, valueAction } = this.props;
+    return (
+      <div className="inputBox">
+        <Input placeholder="请输入待办事项" className="input" value={value} onChange={valueAction} />
+        <Button type="primary" shape="round" onClick={addAction}  >确定</Button>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-      value: state.value
-    }
+  return {
+    value: state.value
   }
-  const mapDispatchToProps = (dispatch) => {
-    return {
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
     //获取输入框的值
-    valueAction:bindActionCreators(valueAction,dispatch),
+    valueAction: bindActionCreators(valueAction, dispatch),
     //确定添加
-    addAction:bindActionCreators(addAction,dispatch)
-    }
+    addAction: bindActionCreators(addAction, dispatch)
   }
+}
 
-export default connect(mapStateToProps,mapDispatchToProps)(InputBox);
+export default connect(mapStateToProps, mapDispatchToProps)(InputBox);
