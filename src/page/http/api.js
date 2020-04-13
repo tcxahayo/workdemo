2ab3413913d238faf6e7d17745eb6d26a6d4e86c
tcx <1392091646@qq.com>
2020-04-13 23:15:40 +0800

    初步封装promise

--- src/page/http/api.js
+++ src/page/http/api.js
@@ -1,11 +1,17 @@
 import axios from 'axios';
 
-export function request(url, params){
+export function request(url='', params={}, method = 'GET'){
     return new Promise((resolve, reject)=>{
-        axios.get(url, params).then(function(response){
+        //只适用于这些请求方法 'PUT', 'POST', 和 'PATCH',要使用data
+        let config = {data:params}
+        //`params` 是即将与请求一起发送的 URL 参数
+        if (method.toUpperCase() === 'GET' || method.toUpperCase() === 'DELETE') {
+            config = { params: params }
+          }
+        axios.request(Object.assign({url, method, params},config)).then((response)=>{
             resolve(response.data)
         })
-        .catch(function(err){
+        .catch((err)=>{
             reject(err)
         })
     })
